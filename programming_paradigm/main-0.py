75283d8725b5fc36c6dd50a40709d9c122185edd
paajoe100 <kojoyankson12@gmail.com>
2024-10-06 16:33:20 +0100

    Add main file

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -9,3 +9,20 @@ def main():
         sys.exit(1)
 
     command, *params = sys.argv[1].split(':')
+    amount = float(params [0]) if params else None
+
+    if command == "deposit" and amount is not None:
+        account.deposit(amount)
+        print(f"Deposited: ${amount}")
+    elif command == "withdraw" and amount is not None:
+        if account.withdraw(amount):
+            print(f"Withdrew: ${amount}")
+        else:
+            print("Insufficient funds.")
+    elif command ==  "display":
+        account.display_balance()
+    else:
+        print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
