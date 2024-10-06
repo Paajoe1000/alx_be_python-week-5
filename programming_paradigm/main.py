fd633d6fafa74a830a3eed0bb13a814de2d819a6
paajoe100 <kojoyankson12@gmail.com>
2024-10-06 21:53:42 +0100

    Add Library import

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -12,3 +12,29 @@ def main():
     print(result)
 if __name__ == "__main__":
     main()
+
+# How to interact with your Book and Library classes.
+from library_management import Book, Library
+
+def main():
+    # Setup a small library
+    library = Library()
+    library.add_book(Book("Brave New World", "Aldous Huxley"))
+    library.add_book(Book("1984", "George Orwell"))
+
+    # Initial list of available books
+    print("Available books after setup:")
+    library.list_available_books()
+
+    #Simu;ate check out a book
+    library.check_out_book("1984")
+    print("\nAvailale books after checking out '1984':")
+    library.list_available_books()
+
+    # Simulate returning a book
+    library.return_book("1984")
+    print("\nAvailble books after returning '1984':")
+    library.list_available_books()
+
+if __name__ == "__main__":
+    main()
