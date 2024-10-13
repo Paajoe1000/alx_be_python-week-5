52e907fd32a88422bdd65c8a4b9ce93bf392acde
paajoe100 <kojoyankson12@gmail.com>
2024-10-13 15:27:46 +0100

    Add library

--- oop/library_system.py
+++ oop/library_system.py
@@ -10,10 +10,14 @@ class Book:
 # Derived Classes
 class EBook(Book):
     def __init__(self, title, author, file_size):
+        super().__init__(title)
+        super().__init__(author)
         self.file_size = int(file_size)
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
+        super().__init__(title)
+        super().__init__(author)
         self.page_count = int(page_count)
 
 # Composition
